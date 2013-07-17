10d45fd1eef3de8d9047bf5b32b1f5ebfd550e0b
Kaleb Hornsby <kaleb@horns.by>
2013-07-17 10:27:11 -0400

    basic setup

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function(){
+    // Allow the collapse nav to be usabel sans-js
+    $('[data-toggle=collapse]').on('click', function(e){
+        e.preventDefault();
+    });
+    $('.btn-navbar[title]').tooltip({placement:'left'});
+	$('.carousel').carousel();
+    /////////////
+    // Social //
+    ////////////
+    $('.follow[title]').tooltip({placement:'bottom'});
+    //User Voice
+    $('.follow .icon-envelope-alt').parents('a').on('click', function(e){
+        UserVoice.showPopupWidget();
+        e.preventDefault();
+    })
+    //Twitter
+    $.getJSON('http://api.twitter.com/1/users/show.json?screen_name=GeorgiaRLC&callback=?', function(data){
+        $('.follow .icon-twitter').parents('li')
+        .attr('data-original-title', data.followers_count + ' followers on Twitter');
+    });
+    //Facebook
+    $.getJSON("https://graph.facebook.com/RLCofGeorgia?callback=?", function(data){
+        $('.follow .icon-facebook').parents('li')
+        .attr('data-original-title', data.likes + ' likes on Facebook');
+    });
+
+    //////////////
+    // JS Shims //
+    //////////////
+    
+    /** Autofocus */
+    if(!Modernizr.input.autofocus) {
+    	$('[autofocus]:first').focus();
+    }
+    // TODO Add input[required] shim
+});
